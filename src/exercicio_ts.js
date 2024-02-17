422fc27648cde3b774e3b9b7a0bf26f8ed2fc317
Gabriel Gomes <gabriel.gomes_fernandes@hotmail.com>
2024-02-17 15:29:38 -0300

    firts commit

---
+++ src/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(number01, number02) {
+    return number01 * number02;
+}
+const saudacao = (name) => {
+    return `Olá, ${name}`;
+};
+console.log(multiplicacao(2, 2));
+console.log(saudacao('gabriel'));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,10 @@
+function multiplicacao(number01: number, number02: number):number {
+    return number01 * number02
+}
+
+const saudacao = (name: string): string => {
+    return `Olá, ${name}`
+}
+
+console.log(multiplicacao(2,2))
+console.log(saudacao('gabriel'))
